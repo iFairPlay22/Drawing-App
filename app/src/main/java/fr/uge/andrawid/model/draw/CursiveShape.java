c3ff26e2a48c3a4e8c12251785f7d702c508e1d2
DavyDng <59774945+DavyDng@users.noreply.github.com>
2020-05-07 11:34:27 +0200

    Ligne cursive début

--- app/src/main/java/fr/uge/andrawid/model/draw/CursiveShape.java
+++ app/src/main/java/fr/uge/andrawid/model/draw/CursiveShape.java
@@ -1,10 +1,22 @@
 package fr.uge.andrawid.model.draw;
 
+import android.graphics.Canvas;
+
+import java.util.Objects;
+
+import fr.uge.andrawid.model.ShapeProperties;
+
 public class CursiveShape extends AbstractDrawableShape {
 
     public CursiveShape(float[] coordinates) {
         super(coordinates);
     }
 
+    @Override
+    public void drawShape(ShapeProperties properties, Canvas canvas) {
+        Objects.requireNonNull(properties);
+        Objects.requireNonNull(canvas);
 
+        canvas
+    }
 }
