852c2e1efd017da042e34c6ab3590c06619485b0
Ewen Bouquet <ewen.bouquet@gmail.com>
2020-05-07 12:54:52 +0200

    Draw all forms --ok

--- app/src/main/java/fr/uge/andrawid/MainActivity.java
+++ app/src/main/java/fr/uge/andrawid/MainActivity.java
@@ -20,33 +20,9 @@ import fr.uge.andrawid.view.DrawingView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ShapeKind selectedShapeKind = ShapeKind.CURSIVE;
     private DrawingView drawingView;
     private ShapeContainer shapeContainer;
     private ShapeBuilder shapeBuilder;
-
-    private float initialX;
-    private float initialY;
-    private ArrayList<Float> cursiveCoords;
-
-    private float[] getCoordinates(float x, float y) {
-        int size = cursiveCoords.size() + 4;
-        float[] coordinates = new float[size];
-
-        coordinates[0] = 0.0f;
-        coordinates[1] = 0.0f;
-
-        int i = 2;
-        for (Float coords : cursiveCoords) {
-            coordinates[i] = coords;
-            i++;
-        }
-
-        coordinates[size - 2] = x - initialX;
-        coordinates[size - 1] = y - initialY;
-
-        return coordinates;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         drawingView = findViewById(R.id.drawingView);
         drawingView.setModel(shapeContainer);
 
+        shapeBuilder.setShapeKind(ShapeKind.CURSIVE);
 
         drawingView.setOnTouchListener(
 
@@ -67,25 +44,19 @@ public class MainActivity extends AppCompatActivity {
 
                         case MotionEvent.ACTION_DOWN:
 
-                            initialX = event.getX();
-                            initialY = event.getY();
-
-                            cursiveCoords = new ArrayList<>();
+                            shapeBuilder.actionDown(event.getX(), event.getY());
 
                             return true;
 
                         case MotionEvent.ACTION_MOVE:
 
-                            if (selectedShapeKind == ShapeKind.CURSIVE) {
-                                cursiveCoords.add(event.getX() - initialX);
-                                cursiveCoords.add(event.getY() - initialY);
-                            }
+                            shapeBuilder.actionMove(event.getX(), event.getY());
 
                             return true;
 
                         case MotionEvent.ACTION_UP:
 
-                            shapeContainer.add(shapeBuilder.build(getCoordinates(event.getX(), event.getY())), new ShapeProperties(initialX, initialY));
+                            shapeContainer.add(shapeBuilder.actionUp(event.getX(), event.getY()), shapeBuilder.getShapeProperties());
 
                             return true;
 
--- app/src/main/java/fr/uge/andrawid/model/ShapeBuilder.java
+++ app/src/main/java/fr/uge/andrawid/model/ShapeBuilder.java
@@ -1,5 +1,7 @@
 package fr.uge.andrawid.model;
 
+import java.util.ArrayList;
+
 import fr.uge.andrawid.model.draw.CursiveShape;
 import fr.uge.andrawid.model.draw.DrawableShape;
 import fr.uge.andrawid.model.draw.LineShape;
@@ -8,6 +10,10 @@ import fr.uge.andrawid.model.draw.ShapeKind;
 
 public class ShapeBuilder {
     private ShapeKind selectedShapeKind;
+
+    private float initialX;
+    private float initialY;
+    private ArrayList<Float> cursiveCoords;
 
     /** to set the current kind of shape to create */
     public void setShapeKind(ShapeKind shapeKind) {
@@ -20,7 +26,30 @@ public class ShapeBuilder {
      * coords[coords.length-2] and coords[coords.length-1] are the x and y of the end of the shape
      * some shape kinds (like CURSIVE) may admit intermediate points in the array
      */
-    public DrawableShape build(float[] coords) {
+
+    public void actionDown(float x, float y) {
+        initialX = x;
+        initialY = y;
+
+        cursiveCoords = new ArrayList<>();
+    }
+
+    public void actionMove(float x, float y) {
+        if (selectedShapeKind == ShapeKind.CURSIVE) {
+            cursiveCoords.add(x - initialX);
+            cursiveCoords.add(y - initialY);
+        }
+    }
+
+    public DrawableShape actionUp(float x, float y) {
+        return build(buildCoordinates(x, y));
+    }
+
+    public ShapeProperties getShapeProperties() {
+        return new ShapeProperties(initialX, initialY);
+    }
+
+    private DrawableShape build(float[] coords) {
         switch (selectedShapeKind) {
             case SEGMENT:
                 return new LineShape(coords);
@@ -34,5 +63,24 @@ public class ShapeBuilder {
             default:
                 return null;
         }
+    }
+
+    private float[] buildCoordinates(float x, float y) {
+        int size = cursiveCoords.size() + 4;
+        float[] coordinates = new float[size];
+
+        coordinates[0] = 0.0f;
+        coordinates[1] = 0.0f;
+
+        int i = 2;
+        for (Float coords : cursiveCoords) {
+            coordinates[i] = coords;
+            i++;
+        }
+
+        coordinates[size - 2] = x - initialX;
+        coordinates[size - 1] = y - initialY;
+
+        return coordinates;
     }
 }
